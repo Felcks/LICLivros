fe0b2ab90cdc5f6e432ff640ddfb2dace3e2da36
Gabriel Ribeiro <Gabriel Ribeiro>
2016-12-13 03:04:34 -0200

    Inicio do sistema de geração de documentos

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,12 @@ import utilidades.Screen;
 
 import java.util.Locale;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFTable;
+import org.apache.poi.xwpf.usermodel.XWPFTableRow;
+
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
@@ -10,6 +16,12 @@ public class Main {
 	public static void main(String[] args) {
 		Locale.setDefault(new Locale("pt", "BR"));
 		Screen.start();
+		try {
+			createTableTest();
+		} catch (Exception e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 		
 		// INICIAR TEMA
 		try {
@@ -30,5 +42,37 @@ public class Main {
 		
 		new GUIManager();
 		 
+	}
+
+	public static void createTableTest() throws Exception{
+	
+			//Blank Document
+			   XWPFDocument document= new XWPFDocument();
+			        
+			   //Write the Document in file system
+			   FileOutputStream out = new FileOutputStream(
+			   new File("create_table.docx"));
+			        
+			   //create table
+			   XWPFTable table = document.createTable();
+			   //create first row
+			   XWPFTableRow tableRowOne = table.getRow(0);
+			   tableRowOne.getCell(0).setText("col one, row one");
+			   tableRowOne.addNewTableCell().setText("col two, row one");
+			   tableRowOne.addNewTableCell().setText("col three, row one");
+			   //create second row
+			   XWPFTableRow tableRowTwo = table.createRow();
+			   tableRowTwo.getCell(0).setText("col one, row two");
+			   tableRowTwo.getCell(1).setText("col two, row two");
+			   tableRowTwo.getCell(2).setText("col three, row two");
+			   //create third row
+			   XWPFTableRow tableRowThree = table.createRow();
+			   tableRowThree.getCell(0).setText("col one, row three");
+			   tableRowThree.getCell(1).setText("col two, row three");
+			   tableRowThree.getCell(2).setText("col three, row three");
+				
+			   document.write(out);
+			   out.close();
+			   System.out.println("create_table.docx written successully");
 	}
 }
